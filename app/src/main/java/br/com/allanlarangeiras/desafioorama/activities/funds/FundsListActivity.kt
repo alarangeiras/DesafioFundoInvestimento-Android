b74fb17fc92fb73cf102bfcad4315e3fa0fb14c6
Allan Larangeiras <alarangeiras@gmail.com>
2019-05-30 22:18:52 -0300

    criacao da primeira activity definicao dos estilos similares ao app original

---
+++ app/src/main/java/br/com/allanlarangeiras/desafioorama/activities/funds/FundsListActivity.kt
@@ -0,0 +1,13 @@
+package br.com.allanlarangeiras.desafioorama.activities.funds
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.allanlarangeiras.desafioorama.R
+
+class FundsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_funds_list)
+    }
+}
