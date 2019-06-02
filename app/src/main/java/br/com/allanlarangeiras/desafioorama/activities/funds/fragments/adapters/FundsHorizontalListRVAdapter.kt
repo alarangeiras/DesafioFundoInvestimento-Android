c0e406633ceb551df9c3bbe03f22bd0ab6c20ee0
Allan Larangeiras <alarangeiras@gmail.com>
2019-06-01 22:32:48 -0300

    correcao de bugs

--- app/src/main/java/br/com/allanlarangeiras/desafioorama/activities/funds/fragments/adapters/FundsHorizontalListRVAdapter.kt
+++ app/src/main/java/br/com/allanlarangeiras/desafioorama/activities/funds/fragments/adapters/FundsHorizontalListRVAdapter.kt
@@ -1,6 +1,8 @@
 package br.com.allanlarangeiras.desafioorama.activities.funds.fragments.adapters
 
+import android.content.Context
 import android.content.Intent
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -14,15 +16,13 @@ import br.com.allanlarangeiras.desafioorama.services.FundsService
 class FundsHorizontalListRVAdapter(
     val fundsList: List<Fund>): RecyclerView.Adapter<FundsHorizontalListRVAdapter.FundsViewHolder>() {
 
+    private lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): FundsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_fund_horizontal_list, parent, false)
 
-        view.setOnClickListener(View.OnClickListener {
-            val intent = Intent(parent.context, FundDetailActivity::class.java)
-            val fund = fundsList[position]
-            intent.putExtra("fund", fund)
-            parent.context.startActivity(intent)
-        })
+        context = parent.context
+
         return FundsViewHolder(view)
     }
 
@@ -34,6 +34,11 @@ class FundsHorizontalListRVAdapter(
         val fund = fundsList[positon]
         holder.simpleName.text = fund.simpleName
 
+        holder.card.setOnClickListener(View.OnClickListener {
+            val intent = Intent(context, FundDetailActivity::class.java)
+            intent.putExtra("fund", fund)
+            context.startActivity(intent)
+        })
         holder.m12.text = FundsService.formatM12(fund.profitabilities.m12)
         holder.minimumApplication.text = FundsService.formatAmount(fund.operability.minimumInitialApplicationAmount)
 
@@ -46,8 +51,10 @@ class FundsHorizontalListRVAdapter(
         val simpleName: TextView
         val m12: TextView
         val minimumApplication: TextView
+        val card: CardView
 
         init{
+            card = parentView.findViewById<CardView>(R.id.card)
             simpleName = parentView.findViewById<TextView>(R.id.simpleName)
             m12 = parentView.findViewById<TextView>(R.id.m12)
             minimumApplication = parentView.findViewById<TextView>(R.id.minimumApplication)
--- app/src/main/java/br/com/allanlarangeiras/desafioorama/activities/funds/fragments/adapters/TopFundsRVAdapter.kt
+++ app/src/main/java/br/com/allanlarangeiras/desafioorama/activities/funds/fragments/adapters/TopFundsRVAdapter.kt
@@ -1,6 +1,8 @@
 package br.com.allanlarangeiras.desafioorama.activities.funds.fragments.adapters
 
+import android.content.Context
 import android.content.Intent
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -14,14 +16,18 @@ import br.com.allanlarangeiras.desafioorama.services.FundsService
 class TopFundsRVAdapter(
     val topFunds: List<Fund>): RecyclerView.Adapter<TopFundsRVAdapter.TopFundsViewHolder>() {
 
+    private lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): TopFundsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_top_funds_list, null)
-        view.setOnClickListener(View.OnClickListener {
-            val intent = Intent(parent.context, FundDetailActivity::class.java)
-            val fund = topFunds[position]
-            intent.putExtra("fund", fund)
-            parent.context.startActivity(intent)
-        })
+        context = parent.context
+        view.setLayoutParams(
+            RecyclerView.LayoutParams(
+                RecyclerView.LayoutParams.MATCH_PARENT,
+                RecyclerView.LayoutParams.WRAP_CONTENT
+            )
+        )
+
         return TopFundsViewHolder(view)
     }
 
@@ -31,6 +37,11 @@ class TopFundsRVAdapter(
 
     override fun onBindViewHolder(holder: TopFundsViewHolder, position: Int) {
         val fund = topFunds[position]
+        holder.card.setOnClickListener(View.OnClickListener {
+            val intent = Intent(context, FundDetailActivity::class.java)
+            intent.putExtra("fund", fund)
+            context.startActivity(intent)
+        })
         holder.simpleName.text = fund.simpleName
         holder.m12.text = FundsService.formatM12(fund.profitabilities.m12)
         holder.minimumApplication.text = FundsService.formatAmount(fund.operability.minimumInitialApplicationAmount)
@@ -40,11 +51,13 @@ class TopFundsRVAdapter(
     class TopFundsViewHolder(
         private val parent:View): RecyclerView.ViewHolder(parent) {
 
-        var simpleName: TextView
-        var m12: TextView
-        var minimumApplication: TextView
+        val simpleName: TextView
+        val m12: TextView
+        val minimumApplication: TextView
+        val card: CardView
 
         init {
+            card = parent.findViewById<CardView>(R.id.card)
             simpleName = parent.findViewById<TextView>(R.id.simpleName)
             m12 = parent.findViewById<TextView>(R.id.m12)
             minimumApplication = parent.findViewById<TextView>(R.id.minimumApplication)
