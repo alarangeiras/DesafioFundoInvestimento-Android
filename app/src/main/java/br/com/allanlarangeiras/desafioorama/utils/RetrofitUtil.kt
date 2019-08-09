696e239431481a20c317c31a6f7093d89e4a53c3
Allan Larangeiras <alarangeiras@gmail.com>
2019-08-09 20:34:39 -0300

    adicionando timeout de 60 segundos

--- app/src/main/java/br/com/allanlarangeiras/desafioorama/utils/RetrofitUtil.kt
+++ app/src/main/java/br/com/allanlarangeiras/desafioorama/utils/RetrofitUtil.kt
@@ -1,11 +1,12 @@
 package br.com.allanlarangeiras.desafioorama.utils
 
 import br.com.allanlarangeiras.desafioorama.BuildConfig
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 object RetrofitUtil {
@@ -18,7 +19,12 @@ object RetrofitUtil {
     private fun buildRetrofit(baseUrl: String): Retrofit {
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+        val client = OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(interceptor).build()
+
+
 
         return Retrofit.Builder()
             .baseUrl(baseUrl)
